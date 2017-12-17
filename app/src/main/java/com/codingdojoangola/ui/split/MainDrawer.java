301f878b70770d811f29efc0e190739e8e46a4a0
OsvaldoJD <dulo2912@gmail.com>
2017-12-17 16:33:36 +0000

    Layout show information CDa and mission

--- app/src/main/java/com/codingdojoangola/ui/split/MainDrawer.java
+++ app/src/main/java/com/codingdojoangola/ui/split/MainDrawer.java
@@ -46,11 +46,8 @@ public class MainDrawer {
                 ""+R.string.project,
                 ""+R.string.member,
                 ""+R.string.cda,
-<<<<<<< HEAD
                 ""+R.string.info,
-=======
                 ""+R.string.chat,
->>>>>>> 90ebf6c5ff596cfb05bcebb9d49e8db8da542c25
                 ""+R.string.settings,
                 ""+R.string.about,
                 ""+R.string.exit
